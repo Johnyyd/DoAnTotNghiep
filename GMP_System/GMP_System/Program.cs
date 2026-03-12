ba79539fa5b3a1575368b9953a72be33d3a07604
Johnyyd <trinm2102@gmail.com>
2026-03-12 12:54:27 +0700

    Add seed data for materials and recipes

--- GMP_System/GMP_System/Program.cs
+++ GMP_System/GMP_System/Program.cs
@@ -48,6 +48,75 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<GmpContext>();
     db.Database.EnsureCreated();
+
+    // Seed data if empty
+    if (!db.Materials.Any())
+    {
+        var unit1 = new UnitOfMeasure { Code = "KG", Name = "Kilogram", Description = "Kilogram" };
+        var unit2 = new UnitOfMeasure { Code = "G", Name = "Gram", Description = "Gram" };
+        var unit3 = new UnitOfMeasure { Code = "TAB", Name = "Tablet", Description = "Tablet" };
+        db.UnitOfMeasures.AddRange(unit1, unit2, unit3);
+        db.SaveChanges();
+
+        var material1 = new Material
+        {
+            MaterialCode = "MAT-001",
+            MaterialName = "Paracetamol 500mg",
+            Type = "RawMaterial",
+            BaseUomId = unit3.UnitId,
+            IsActive = true,
+            Description = "Active ingredient for pain relief",
+            CreatedAt = DateTime.Now
+        };
+        var material2 = new Material
+        {
+            MaterialCode = "MAT-002",
+            MaterialName = "Microcrystalline Cellulose",
+            Type = "RawMaterial",
+            BaseUomId = unit1.UnitId,
+            IsActive = true,
+            Description = "Excipient binder",
+            CreatedAt = DateTime.Now
+        };
+        var material3 = new Material
+        {
+            MaterialCode = "MAT-003",
+            MaterialName = "Para Film",
+            Type = "Packaging",
+            BaseUomId = unit2.UnitId,
+            IsActive = true,
+            Description = "Packaging film for blisters",
+            CreatedAt = DateTime.Now
+        };
+        db.Materials.AddRange(material1, material2, material3);
+
+        var recipe1 = new Recipe
+        {
+            MaterialId = material1.MaterialId,
+            BatchSize = 1000,
+            Status = "Draft",
+            VersionNumber = 1,
+            CreatedAt = DateTime.Now,
+            EffectiveDate = DateTime.Now.AddDays(1),
+            Note = "Initial draft"
+        };
+        db.Recipes.Add(recipe1);
+        db.SaveChanges();
+
+        // Add recipe BOM
+        if (recipe1.RecipeId > 0)
+        {
+            db.RecipeBoms.Add(new RecipeBom
+            {
+                RecipeId = recipe1.RecipeId,
+                MaterialId = material2.MaterialId,
+                Quantity = 0.5m,
+                UomId = unit1.UnitId
+            });
+        }
+
+        db.SaveChanges();
+    }
 }
 
 // Configure the HTTP request pipeline.
