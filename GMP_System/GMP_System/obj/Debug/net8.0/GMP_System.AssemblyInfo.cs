0d81c52e5dbe415bfa693e07e4594573edc7b25f
Tun <thudientu782004@gmail.com>
2026-05-25 14:14:02 +0700

    Sao lưu và khôi phục database

--- GMP_System/GMP_System/obj/Debug/net8.0/GMP_System.AssemblyInfo.cs
+++ GMP_System/GMP_System/obj/Debug/net8.0/GMP_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GMP_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf2541f2ecf0e1b6a731c0dc3d1100e4973e0add")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9345b710d40a936d1d07b26aaf4c5c9a2161d7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GMP_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GMP_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
