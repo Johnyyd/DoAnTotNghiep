f680cc42c320e4a1018899d566cbfd66e776a897
Tun <thudientu782004@gmail.com>
2026-05-28 09:35:59 +0700

    quick demo

--- GMP_System/GMP_System/obj/Debug/net8.0/GMP_System.AssemblyInfo.cs
+++ GMP_System/GMP_System/obj/Debug/net8.0/GMP_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GMP_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7f697cbba3f1decc59d3432b2e12d9d9840797")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GMP_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GMP_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
